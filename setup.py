5ab911fe66b2a7dbb6aa1ece47473c7ea641375a
ax <ax.tryin@gmail.com>
2019-05-10 15:40:08 +0200

    pyPI readme

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
   name = 'dye',
   packages = ['dye'],
-  version = '0.2',
+  version = '0.3',
   description = 'Lightweight module with ANSI control codes to dye python scripts, the simplest and lightest module to work with colors and formatting.',
   long_description=long_description,
   long_description_content_type="text/markdown",
   author = 'ax',
   author_email = 'ax.tryin@gmail.com',
   url = 'https://github.com/ax-tryin/dye',
-  download_url = 'https://github.com/ax-tryin/dye/archive/0.2.tar.gz',
+  download_url = 'https://github.com/ax-tryin/dye/archive/0.3.tar.gz',
   keywords = ['ANSI', 'color', 'terminal', 'lightweight']
-  setup_cfg=True
 )
