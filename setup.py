4d290573918579fa20d6789008b7e97c955ad62f
ax <ax.tryin@gmail.com>
2019-05-24 19:27:31 +0200

    update release version for pyPI (fixin README for pyPI.org)

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.rst", "r") as fh:
 setup(
   name = 'dye',
   packages = ['dye'],
-  version = '0.3',
+  version = '0.4',
   description = 'Lightweight module with ANSI control codes to dye python scripts, the simplest and lightest module to work with colors and formatting.',
   long_description=long_description,
   author = 'ax',
   author_email = 'ax.tryin@gmail.com',
   url = 'https://github.com/ax-tryin/dye',
-  download_url = 'https://github.com/ax-tryin/dye/archive/0.3.tar.gz',
+  download_url = 'https://github.com/ax-tryin/dye/archive/0.4.tar.gz',
   keywords = ['ANSI', 'color', 'terminal', 'lightweight']
 )
